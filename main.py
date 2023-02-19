8d85f420c2886ee013b1f92724b72db9fbfa4b98
박한 <bagkan380@gmail.com>
2023-02-19 11:43:28 +0900

    introduce

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 import school
 
 a = school.Student('kim',1)
-b = school.Student("lee",2)
-c = school.Student("park",3)
+mathTeacher = school.Teacher('kim','math')
 
-a.greeting()
-
-
-mathTeacher = school.Teacher('math','수학선생님')
-enTeacher = school.Teacher('english','영어선생님')
-
-mathTeacher.greeting()
-enTeacher.greeting()
-
-ban1 = school.Class(1,"선생님1","학생1")
+ban1 = school.Class(mathTeacher,1)
+ban1.introducing()
+ban1.addstudent(a)
+ban1.introducing()
--- school.py
+++ school.py
@@ -2,9 +2,16 @@ class Student:
     def __init__(self,name,grade):
         self.name = name
         self.grade = grade
+        self.math = 0
+        self.english = 0
 
+    def studyMath(self):
+        print("수학공부했습니다 수학 능력이 10 올랐어요")
+        self.math += 10
     def greeting(self):
-        print(f"안녕하세요 저는 {self.name}입니다 저의 학년은 {self.grade}학년 입니다")
+        print(f"안녕하세요 저는 {self.name}입니다 저의 학년은 {self.grade}학년 입니다 저의 수학 능력은 {self.math}입니다 저의 영어 능력은 {self.english}입니다")
+
+
 
 class Teacher:
     def __init__(self,sub,name):
@@ -15,10 +22,17 @@ class Teacher:
         print(f"안녕하세요 저는 {self.name}입니다 제 담당 과목은 {self.sub}입니다")
 
 class Class:
-    def __init__(self,teacher,student, ban):
+    def __init__(self,teacher, ban):
         self.teacher = teacher
-        self.student = student
+        self.student = []
         self.ban = ban
 
+    def addstudent(self,student):
+        self.student.append(student)
+
+
     def introducing(self):
-        print(f"{self.ban}반의 담당 선생님은{self.teacher}이고 학생은{self.student}이다 ")
+        nameList = []
+        for i in self.student:
+            nameList.append(i.name)
+        print(f"{self.ban}반의 담당 선생님은{self.teacher.name}이고 학생은{nameList}이다 ")
